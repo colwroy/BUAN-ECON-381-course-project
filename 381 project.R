505423e0d6e4e031a5c2be0e0e192a09aa3fcf2d
colwroy <croy@sandiego.edu>
2026-04-26 19:43:00 -0700

    Add files via upload

---
+++ 381 project.R
@@ -0,0 +1,73 @@
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+
+#load data
+housing1 <- read.csv("https://raw.githubusercontent.com/colwroy/BUAN-ECON-381-course-project/refs/heads/main/Housing.csv")
+summary(housing1)
+
+#binary variables changed from yes/no to 1 and 0
+housing1$mainrbinary <- ifelse(housing1$mainroad == 'yes', 1, 0)
+housing1$groombinary <- ifelse(housing1$guestroom == 'yes', 1, 0)
+housing1$basementbinary <- ifelse(housing1$basement == 'yes', 1, 0)
+housing1$hwaterbinary <- ifelse(housing1$hotwaterheating == 'yes', 1, 0)
+housing1$acbinary <- ifelse(housing1$airconditioning == 'yes', 1, 0)
+housing1$prefareabinary <- ifelse(housing1$prefarea == 'yes', 1, 0)
+
+#create new DF with new binary columns
+housing2 <- housing1[1:5]
+housing2 <- cbind(housing2,housing1[11])
+housing2 <- cbind(housing2,housing1[14:19])
+
+#correlation matrix
+#strongest correlates with price are area, number of bathrooms,
+#number of stories, and acbinary
+cor(housing2)
+
+
+#split the data first into training (70%) and holdout (30%)
+set.seed(123)
+housing2$randnum <- runif(dim(housing2)[1], min = 0, max = 1)
+housing2_training <- housing2[housing2$randnum<0.7,]
+housing2_holdout <- housing2[housing2$randnum>0.7,]
+
+#split holdout into testing (50%) and validation (50%)
+housing2_holdout$randnum <- runif(dim(housing2_holdout)[1], min = 0, max = 1)
+housing2_testing <- housing2_holdout[housing2_holdout$randnum<0.5,]
+housing2_validation <- housing2_holdout[housing2_holdout$randnum>0.5,]
+
+#export split data for sharing in CSV format
+write.csv(housing2_training, "housing2_training.csv", row.names = TRUE)
+write.csv(housing2_testing, "housing2_testing.csv", row.names = TRUE)
+write.csv(housing2_validation, "housing2_validation.csv", row.names = TRUE)
+
+#regression
+############################################################################
+
+#bivariate model using house area and price
+priceVarea <- lm(price~area, data = housing2_training)
+
+#make predictions and use them to plot a regression line on a scatter plot
+housing2_training$pred <- predict(priceVarea, housing2_training)
+ggplot(housing2_training, aes(x = area, y = price)) + geom_point() + geom_line(aes(y = pred), color = "purple")
+
+summary(priceVarea)
+cor(housing2_training)
+
+#level log transform
+#B1/100 * change in x1 = change in y
+priceVarea_LogLog <- lm(price~log(area), data = housing2_training)
+housing2_training$pred_LogLog <- predict(priceVarea_LogLog, housing2_training)
+ggplot(housing2_training, aes(x = area, y = price)) + geom_point() + geom_line(aes(y = pred_LogLog), color = "purple")
+
+summary(priceVarea_LogLog)
+
+
+
+
+
+#logistic classification model
+summary(glm(acbinary~price+stories,data=housing1, family = "binomial"))
+
+
+
